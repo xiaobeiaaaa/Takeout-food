321e6b32e30609402a65c7267fa7951d42bca3a0
codermast <dongpeng@123>
2022-11-26 16:56:20 +0800

    扫描过滤器注解

--- src/main/java/com/codermast/takeoutfood/TakeoutFoodApplication.java
+++ src/main/java/com/codermast/takeoutfood/TakeoutFoodApplication.java
@@ -3,9 +3,11 @@ package com.codermast.takeoutfood;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan("com.codermast.takeoutfood.filter")
 public class TakeoutFoodApplication {
 
     public static void main(String[] args) {
