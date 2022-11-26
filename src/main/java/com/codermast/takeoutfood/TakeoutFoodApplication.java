a3b5fe1748e5355cb39d6b663cc787650de09b16
codermast <dongpeng@123>
2022-11-26 09:33:51 +0800

    Initial commit

---
+++ src/main/java/com/codermast/takeoutfood/TakeoutFoodApplication.java
@@ -0,0 +1,13 @@
+package com.codermast.takeoutfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeoutFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeoutFoodApplication.class, args);
+    }
+
+}
