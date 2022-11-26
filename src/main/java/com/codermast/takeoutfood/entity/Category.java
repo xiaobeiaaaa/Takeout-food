b022a3101510747413e6ddf29110e26041ee9ff3
codermast <codermast@qq.com>
2022-11-26 23:15:37 +0800

    分类实体

---
+++ src/main/java/com/codermast/takeoutfood/entity/Category.java
@@ -0,0 +1,55 @@
+package com.codermast.takeoutfood.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @Description: 分类实体
+ * @author: CoderMast
+ * @date: 2022/11/26
+ * @Blog: <a href="https://www.codermast.com/">codermast</a>
+ */
+@Data
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+
+    //类型 1 菜品分类 2 套餐分类
+    private Integer type;
+
+
+    //分类名称
+    private String name;
+
+
+    //顺序
+    private Integer sort;
+
+
+    //创建时间
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+
+    //更新时间
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    //创建人
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+
+    //修改人
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+
+}
