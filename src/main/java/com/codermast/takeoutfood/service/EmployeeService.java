fd2cf5c1adbdef59082fc28bb6cdffb64f395484
codermast <dongpeng@123>
2022-11-26 11:12:01 +0800

    用户服务接口

---
+++ src/main/java/com/codermast/takeoutfood/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.codermast.takeoutfood.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.codermast.takeoutfood.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
