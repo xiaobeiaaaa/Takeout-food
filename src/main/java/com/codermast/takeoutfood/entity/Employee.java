0f770b06a74af8e6a8f5986775990962a63e3167
codermast <codermast@qq.com>
2022-11-27 10:00:38 +0800

    员工实体添加公共字段处理

--- src/main/java/com/codermast/takeoutfood/entity/Employee.java
+++ src/main/java/com/codermast/takeoutfood/entity/Employee.java
@@ -1,6 +1,8 @@
 package com.codermast.takeoutfood.entity;
 
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -53,17 +55,21 @@ public class Employee implements Serializable {
     /**
     * 创建时间
     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime createTime;
     /**
     * 更新时间
     */
+    @TableField(fill = FieldFill.UPDATE)
     private LocalDateTime updateTime;
     /**
     * 创建人
     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long createUser;
     /**
     * 修改人
     */
+    @TableField(fill = FieldFill.UPDATE)
     private Long updateUser;
 }
