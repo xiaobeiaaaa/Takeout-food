c7c747f17225fa65ae45563464ff9627b29ff413
codermast <codermast@qq.com>
2022-11-27 20:56:24 +0800

    套餐业务实现类

---
+++ src/main/java/com/codermast/takeoutfood/service/impl/SetMealServiceImpl.java
@@ -0,0 +1,17 @@
+package com.codermast.takeoutfood.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.codermast.takeoutfood.entity.SetMeal;
+import com.codermast.takeoutfood.mapper.SetMealMapper;
+import com.codermast.takeoutfood.service.SetMealService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: 套餐服务实现类
+ * @author: CoderMast
+ * @date: 2022/11/27
+ * @Blog: <a href="https://www.codermast.com/">codermast</a>
+ */
+@Service
+public class SetMealServiceImpl extends ServiceImpl<SetMealMapper, SetMeal> implements SetMealService {
+}
