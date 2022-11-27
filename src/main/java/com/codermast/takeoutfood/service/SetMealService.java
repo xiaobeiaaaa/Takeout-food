43d5e5db8ab416fc0a03662a2ac066c01bbd7955
codermast <codermast@qq.com>
2022-11-27 20:56:17 +0800

    套餐业务接口

---
+++ src/main/java/com/codermast/takeoutfood/service/SetMealService.java
@@ -0,0 +1,7 @@
+package com.codermast.takeoutfood.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.codermast.takeoutfood.entity.SetMeal;
+
+public interface SetMealService extends IService<SetMeal> {
+}
