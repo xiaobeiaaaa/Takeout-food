ed654cf284c18ec4f22e6661dbf0d7cce442e5c7
codermast <codermast@qq.com>
2022-11-27 20:56:00 +0800

    套餐实体

---
+++ src/main/java/com/codermast/takeoutfood/entity/SetMeal.java
@@ -0,0 +1,73 @@
+package com.codermast.takeoutfood.entity;
+
+/**
+ * @Description: 套餐实体
+ * @author: CoderMast
+ * @date: 2022/11/27
+ * @Blog: <a href="https://www.codermast.com/">codermast</a>
+ */
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * 套餐
+ */
+@Data
+@TableName("setmeal")
+public class SetMeal implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+
+    //分类id
+    private Long categoryId;
+
+
+    //套餐名称
+    private String name;
+
+
+    //套餐价格
+    private BigDecimal price;
+
+
+    //状态 0:停用 1:启用
+    private Integer status;
+
+
+    //编码
+    private String code;
+
+
+    //描述信息
+    private String description;
+
+
+    //图片
+    private String image;
+
+
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+}
