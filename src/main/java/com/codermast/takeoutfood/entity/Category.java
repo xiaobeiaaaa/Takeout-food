35541b7fb4b62abf6391834151ff9e140f517fe0
codermast <codermast@qq.com>
2022-11-27 10:02:15 +0800

    分类实体添加公共字段处理

--- src/main/java/com/codermast/takeoutfood/entity/Category.java
+++ src/main/java/com/codermast/takeoutfood/entity/Category.java
@@ -2,6 +2,7 @@ package com.codermast.takeoutfood.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -14,20 +15,18 @@ import java.time.LocalDateTime;
  * @Blog: <a href="https://www.codermast.com/">codermast</a>
  */
 @Data
+@TableName("category")
 public class Category implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
 
-
     //类型 1 菜品分类 2 套餐分类
     private Integer type;
 
-
     //分类名称
     private String name;
-
 
     //顺序
     private Integer sort;
@@ -37,16 +36,13 @@ public class Category implements Serializable {
     @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
-
     //更新时间
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
-
     //创建人
     @TableField(fill = FieldFill.INSERT)
     private Long createUser;
-
 
     //修改人
     @TableField(fill = FieldFill.INSERT_UPDATE)
