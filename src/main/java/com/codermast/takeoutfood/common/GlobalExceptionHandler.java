56552c009d234d4fe3f41782fa4b50d370ebb3a2
codermast <codermast@qq.com>
2022-11-27 15:59:46 +0800

    处理全局异常：新增对业务异常的处理

--- src/main/java/com/codermast/takeoutfood/common/GlobalExceptionHandler.java
+++ src/main/java/com/codermast/takeoutfood/common/GlobalExceptionHandler.java
@@ -22,16 +22,27 @@ public class GlobalExceptionHandler {
      * @Author: CoderMast <a href="https://www.codermast.com/">...</a>
      */
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException exception){
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException exception) {
         log.info("SQLIntegrityConstraintViolationException异常处理");
         log.info(exception.getMessage());
 
         // 异常信息中包含"Duplicate entry"则说明是唯一约束异常
-        if (exception.getMessage().contains("Duplicate entry")){
+        if (exception.getMessage().contains("Duplicate entry")) {
             String[] s = exception.getMessage().split(" ");
             String msg = s[2] + "用户名已存在";
             return R.error(msg);
         }
         return R.error("未知异常");
+    }
+
+    /**
+     * @Description: 业务异常的全局处理
+     * @param customException 业务异常
+     * @Author: CoderMast <a href="https://www.codermast.com/">...</a>
+     */
+    @ExceptionHandler(CustomException.class)
+    public R<String> CustomExceptionHandler(CustomException customException) {
+        log.error("CustomException异常处理");
+        return R.error(customException.getMessage());
     }
 }
