ff276b4ce09b97cf835cf6979c17f5fee6f84f38
codermast <codermast@qq.com>
2022-11-27 11:27:13 +0800

    分类service

---
+++ src/main/java/com/codermast/takeoutfood/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.codermast.takeoutfood.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.codermast.takeoutfood.entity.Category;
+
+/**
+ * @Description: 分类服务接口
+ * @author: CoderMast
+ * @date: 2022/11/27
+ * @Blog: <a href="https://www.codermast.com/">codermast</a>
+ */
+public interface CategoryService extends IService<Category> {
+}
