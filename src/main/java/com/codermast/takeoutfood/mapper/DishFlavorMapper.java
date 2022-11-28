c5fe65c99689f0dfb0d5f767c5ce66f0a74815f1
codermast <codermast@qq.com>
2022-11-28 15:23:49 +0800

    DishFlavorMapper

---
+++ src/main/java/com/codermast/takeoutfood/mapper/DishFlavorMapper.java
@@ -0,0 +1,9 @@
+package com.codermast.takeoutfood.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.codermast.takeoutfood.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DishFlavorMapper extends BaseMapper<DishFlavor> {
+}
