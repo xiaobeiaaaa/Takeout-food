e82a6404b5146909082a792c7674c979e20f3ccf
codermast <codermast@qq.com>
2022-11-28 15:23:56 +0800

    DishFlavorService

---
+++ src/main/java/com/codermast/takeoutfood/service/DishFlavorService.java
@@ -0,0 +1,7 @@
+package com.codermast.takeoutfood.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.codermast.takeoutfood.entity.DishFlavor;
+
+public interface DishFlavorService extends IService<DishFlavor> {
+}
