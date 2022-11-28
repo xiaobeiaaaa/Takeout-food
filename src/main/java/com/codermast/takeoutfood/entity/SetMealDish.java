5ffa92fe0116d427a3223b00a365107439ad8f85
codermast <codermast@qq.com>
2022-11-28 21:46:28 +0800

    套餐

--- src/main/java/com/codermast/takeoutfood/entity/SetMealDish.java
+++ src/main/java/com/codermast/takeoutfood/entity/SetMealDish.java
@@ -2,6 +2,7 @@ package com.codermast.takeoutfood.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
  * 套餐菜品关系
  */
 @Data
+@TableName("setmeal_dish")
 public class SetMealDish implements Serializable {
 
     private static final long serialVersionUID = 1L;
