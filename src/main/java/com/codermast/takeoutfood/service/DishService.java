e28ae13753205d68436d4c860e339eeebaab6e59
codermast <codermast@qq.com>
2022-11-28 15:25:02 +0800

    DishService

--- src/main/java/com/codermast/takeoutfood/service/DishService.java
+++ src/main/java/com/codermast/takeoutfood/service/DishService.java
@@ -1,7 +1,9 @@
 package com.codermast.takeoutfood.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.codermast.takeoutfood.dto.DishDto;
 import com.codermast.takeoutfood.entity.Dish;
 
 public interface DishService extends IService<Dish> {
+    void saveWithFlavor(DishDto dishDto);
 }
