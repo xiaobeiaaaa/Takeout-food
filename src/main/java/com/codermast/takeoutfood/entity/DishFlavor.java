a1e41f27c582876495d02973314756718aeee59f
codermast <codermast@qq.com>
2022-11-28 15:24:55 +0800

    DishFlavor

---
+++ src/main/java/com/codermast/takeoutfood/entity/DishFlavor.java
@@ -0,0 +1,53 @@
+package com.codermast.takeoutfood.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @Description: 菜品口味
+ * @author: CoderMast
+ * @date: 2022/11/28
+ * @Blog: <a href="https://www.codermast.com/">codermast</a>
+ */
+@Data
+public class DishFlavor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+
+    //菜品id
+    private Long dishId;
+
+
+    //口味名称
+    private String name;
+
+
+    //口味数据list
+    private String value;
+
+
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+
+    //是否删除
+    private Integer isDeleted;
+}
