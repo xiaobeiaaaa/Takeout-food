4bf5c9f3fbebf8f903f99e5a4ddcdda2a2fd63c2
codermast <codermast@qq.com>
2022-11-28 21:44:10 +0800

    菜品控制器

--- src/main/java/com/codermast/takeoutfood/controller/DishController.java
+++ src/main/java/com/codermast/takeoutfood/controller/DishController.java
@@ -6,10 +6,12 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.codermast.takeoutfood.common.BaseContext;
 import com.codermast.takeoutfood.common.R;
 import com.codermast.takeoutfood.dto.DishDto;
+import com.codermast.takeoutfood.entity.Category;
 import com.codermast.takeoutfood.entity.Dish;
-import com.codermast.takeoutfood.service.DishFlavorService;
+import com.codermast.takeoutfood.service.CategoryService;
 import com.codermast.takeoutfood.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Description: 菜品管理控制器
@@ -31,10 +34,10 @@ public class DishController {
     // 注入dish业务实体
     @Autowired
     private DishService dishService;
-
-    // 注入dishFlavor实体
+    
+    // 注入category实体
     @Autowired
-    private DishFlavorService dishFlavorService;
+    private CategoryService categoryService;
 
     /**
      * @param page     页码
@@ -44,20 +47,52 @@ public class DishController {
      * @Author: <a href="https://www.codermast.com/">CoderMast</a>
      */
     @GetMapping("/page")
-    public R<Page> page(int page, int pageSize, String name) {
-        Page pageInfo = new Page(page, pageSize);
+    public R<Page<DishDto>> page(int page, int pageSize, String name) {
+        // 菜品分页页面
+        Page<Dish> dishPage = new Page<>(page,pageSize);
+        // 菜品分页交互对象页面
+        Page<DishDto> dishDtoPage = new Page<>();
 
         // 构造条件过滤器
         LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
         // 构建查询条件
         queryWrapper.like(StringUtils.isNotEmpty(name), Dish::getName, name);
+        // 分页查询菜品
+        dishService.page(dishPage, queryWrapper);
 
-        // 增加排序方法
-        queryWrapper.orderByDesc(Dish::getSort);
+        // 菜品分页记录值
+        List<Dish> recordsDish = dishPage.getRecords();
 
-        dishService.page(pageInfo, queryWrapper);
+        // 菜品分页交互对象记录值
+        List<DishDto> recordsDishDto = recordsDish.stream().map((item) -> {
+            // 创建dishDto对象
+            DishDto dishDto = new DishDto();
 
-        return R.success(pageInfo);
+            // 将Dish类型的item属性赋值到dishDto
+            BeanUtils.copyProperties(item,dishDto);
+
+            // 获取对象的分类id
+            Long categoryId = dishDto.getCategoryId();
+
+            // 根据分类id查分类对象
+            Category categoryServiceById = categoryService.getById(categoryId);
+
+            // 从分类对象中取出分类名称
+            String categoryServiceByIdName = categoryServiceById.getName();
+
+            // 设置分类名称到dishDto对象
+            dishDto.setCategoryName(categoryServiceByIdName);
+
+
+            // 返回该对象
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        // 将封装好的记录值赋值给dishDtoPage对象
+        dishDtoPage.setRecords(recordsDishDto);
+        // 将总条数赋值给dishDtoPage
+        dishDtoPage.setTotal(dishPage.getTotal());
+        return R.success(dishDtoPage);
     }
 
     /**
@@ -79,15 +114,8 @@ public class DishController {
      * @Author: CoderMast <a href="https://www.codermast.com/">codermast</a>
      */
     @GetMapping("/{id}")
-    public R<Dish> getOne(@PathVariable String id) {
-        // 构建查询器
-        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
-        // 设置查询条件
-        queryWrapper.eq(Dish::getId, id);
-
-        // 获取查询值
-        Dish dish = dishService.getOne(queryWrapper);
-        return R.success(dish);
+    public R<DishDto> getOne(@PathVariable String id) {
+       return R.success(dishService.getByIdWithFlavor(id));
     }
 
     /**
@@ -103,5 +131,53 @@ public class DishController {
         dishService.saveWithFlavor(dishDto);
 
         return R.success("菜品添加成功");
+    }
+
+    /**
+     * @Description: 修改菜品信息
+     * @param dishDto 菜品的信息
+     * @Author: <a href="https://www.codermast.com/">CoderMast</a>
+     */
+    @PutMapping
+    public R<String> update(@RequestBody DishDto dishDto,HttpServletRequest request){
+        long id = (long) request.getSession().getAttribute("employee");
+        BaseContext.setCurrentId(id);
+        boolean ret = dishService.updateById(dishDto);
+        return ret? R.success("更新成功"):R.error("更新失败");
+    }
+
+    /**
+     * @Description: 停售和启售
+     * @param status : 状态码，0为停售，1为启售
+     * @param ids 操作菜品的id列表
+     * @Author: <a href="https://www.codermast.com/">CoderMast</a>
+     */
+    @PostMapping("/status/{status}")
+    public R<String> status(@PathVariable Integer status,@RequestParam List<Long> ids,HttpServletRequest request){
+        Long id = (Long) request.getSession().getAttribute("employee");
+        BaseContext.setCurrentId(id);
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+
+        queryWrapper.in(ids != null,Dish::getId,ids);
+
+        List<Dish> list = dishService.list(queryWrapper);
+
+        for (Dish dish : list) {
+            if (dish != null){
+                dish.setStatus(status);
+                dishService.updateById(dish);
+            }
+        }
+        return R.success(status == 1? "启售成功" : "停售成功");
+    }
+
+    /**
+     * @Description: 根据分类id查询其下的菜品
+     * @param categoryId 分类id
+     * @Author: <a href="https://www.codermast.com/">CoderMast</a>
+     */
+    @GetMapping("/list")
+    public R<List<Dish>> getListByCategoryIdWithDish(String categoryId){
+        return R.success(dishService.getListByCategoryIdWithDish(categoryId));
     }
 }
