a040752be13496a4670a1544a37964a7482dad0a
codermast <codermast@qq.com>
2022-11-28 15:25:10 +0800

    DishServiceImpl

--- src/main/java/com/codermast/takeoutfood/service/impl/DishServiceImpl.java
+++ src/main/java/com/codermast/takeoutfood/service/impl/DishServiceImpl.java
@@ -1,10 +1,17 @@
 package com.codermast.takeoutfood.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.codermast.takeoutfood.dto.DishDto;
 import com.codermast.takeoutfood.entity.Dish;
+import com.codermast.takeoutfood.entity.DishFlavor;
 import com.codermast.takeoutfood.mapper.DishMapper;
+import com.codermast.takeoutfood.service.DishFlavorService;
 import com.codermast.takeoutfood.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @Description: 菜品管理服务实现类
@@ -14,4 +21,30 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+    @Autowired
+    DishFlavorService dishFlavorService;
+
+    @Autowired
+    DishService dishService;
+
+    /**
+     * @Description: 新建菜品和口味
+     * @param dishDto 封装实体
+     * @Author: <a href="https://www.codermast.com/">CoderMast</a>
+     */
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDto dishDto){
+        List<DishFlavor> flavors = dishDto.getFlavors();
+
+        for (DishFlavor flavor : flavors) {
+            flavor.setDishId(dishDto.getCategoryId());
+        }
+
+        // 批量保存菜品偏好
+        dishFlavorService.saveBatch(flavors);
+
+        // 添加菜品
+        dishService.save(dishDto);
+    }
 }
