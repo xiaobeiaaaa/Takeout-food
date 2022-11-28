dfd0a9a4318e9dfe21950dea115d5c3991ea6ced
codermast <codermast@qq.com>
2022-11-28 15:24:45 +0800

    DishDto

---
+++ src/main/java/com/codermast/takeoutfood/dto/DishDto.java
@@ -0,0 +1,19 @@
+package com.codermast.takeoutfood.dto;
+
+import com.codermast.takeoutfood.entity.Dish;
+import com.codermast.takeoutfood.entity.DishFlavor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class DishDto extends Dish {
+
+    //菜品对应的口味数据
+    private List<DishFlavor> flavors = new ArrayList<>();
+
+    private String categoryName;
+
+    private Integer copies;
+}
