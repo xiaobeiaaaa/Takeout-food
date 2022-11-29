6d499678d36f2296984445fc80592cf5ca60cbc1
codermast <codermast@qq.com>
2022-11-29 22:13:05 +0800

    用户Service

---
+++ src/main/java/com/codermast/takeoutfood/service/UserService.java
@@ -0,0 +1,7 @@
+package com.codermast.takeoutfood.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.codermast.takeoutfood.entity.User;
+
+public interface UserService extends IService<User> {
+}
